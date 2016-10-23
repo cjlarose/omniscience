070c474168bf93f5b06a7ecc5cea1cd3eaa4a572
Chris LaRose <cjlarose@gmail.com>
2016-10-22 22:03:21 -0700

    Fetching events. Maybe not enough events

--- github-ingestion/src/main.js
+++ github-ingestion/src/main.js
@@ -1,1 +1,53 @@
-console.log('test');
+const GithubApi = require('github');
+const Bluebird = require('bluebird');
+
+const authToken = process.env.AUTH_TOKEN;
+if (!authToken) {
+  console.error('Missing AUTH_TOKEN');
+  process.exit(1);
+}
+
+const githubOwner = process.env.GITHUB_OWNER;
+if (!githubOwner) {
+  console.error('Missing GITHUB_OWNER');
+  process.exit(1);
+}
+const githubRepo = process.env.GITHUB_REPO;
+if (!githubRepo) {
+  console.error('Missing GITHUB_REPO');
+  process.exit(1);
+}
+
+const github = new GithubApi({ Promise: Bluebird });
+
+github.authenticate({
+  type: "oauth",
+  token: authToken
+});
+
+async function getEvents(owner, repo, newerThanEventId = undefined) {
+  const reqOptions = { owner, repo, page: 1, per_page: 100 };
+  const events = [];
+
+  let resp = await github.activity.getEventsForRepo(reqOptions);
+  while (true) {
+    for (let i = 0; i < resp.length; i++) {
+      const event = resp[i];
+      events.push(event);
+    }
+
+    if (github.hasNextPage(resp)) {
+      resp = await github.getNextPage(resp, {});
+      console.log(resp);
+    } else {
+      break;
+    }
+  }
+  return events;
+}
+
+getEvents(githubOwner, githubRepo)
+.then((events) => {
+  console.log(events.length);
+}, (err) => {
+});
