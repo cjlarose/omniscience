492d292279d852a7d2ecae6da64264e2167ab866
Chris LaRose <cjlarose@gmail.com>
2016-10-31 22:30:37 -0700

    Detect push events for unreviewed commits

--- consumers/src/reducers/push_event_annotation.js
+++ consumers/src/reducers/push_event_annotation.js
@@ -16,22 +16,42 @@ function updateRepo(state, repo, f) {
   return Object.assign({}, state, { repos: newRepos });
 }
 
+function pushEventOnWatchedRef(event) {
+  // TODO: Allow git-flow config here
+  return event.payload.ref === 'refs/heads/develop' ||
+    event.payload.ref.startsWith('refs/heads/release/');
+}
+
 function annotatePushEvent(state = initialState, action) {
   switch (action.type) {
     case 'PushEvent': {
-      const repo = action.event.repo.id;
-      return updateRepo(state, repo, oldRepoState =>
-        Object.assign({}, oldRepoState, { numPushEvents: oldRepoState.numPushEvents + 1 })
-      );
+      const { id: repoId, name: repoName } = action.event.repo;
+      const payload = action.event.payload;
+
+      if (!pushEventOnWatchedRef(action.event)) {
+        return state;
+      }
+      return updateRepo(state, repoId, (oldRepoState) => {
+        if (oldRepoState.mergedPrs[payload.head]) {
+          console.log(`${repoName}: Push event for PR #${oldRepoState.mergedPrs[payload.head]} observed!`);
+        } else {
+          console.error(`${repoName}: Commit ${payload.head} onto ${payload.ref} does not appear to be from a PR`);
+          console.log('Known PRs');
+          console.log(JSON.stringify(oldRepoState.mergedPrs, null, 2));
+        }
+
+        return Object.assign({}, oldRepoState, { numPushEvents: oldRepoState.numPushEvents + 1 });
+      });
     }
     case 'PullRequestEvent': {
-      const repo = action.event.repo.id;
+      const { id: repoId, name: repoName } = action.event.repo;
       const payload = action.event.payload;
       if (payload.action !== 'closed') {
         return state;
       }
-      return updateRepo(state, repo, (oldRepoState) => {
+      return updateRepo(state, repoId, (oldRepoState) => {
         if (payload.pull_request.merged) {
+          console.log(`${repoName}: PR #${payload.pull_request.number} merged!`);
           return Object.assign({}, oldRepoState, {
             mergedPrs: Object.assign({}, oldRepoState.mergedPrs, {
               [payload.pull_request.merge_commit_sha]: payload.pull_request.number,
