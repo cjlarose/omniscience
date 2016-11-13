341f7e87ef43f604a1b65e93b9673fcec45b74a8
Chris LaRose <cjlarose@gmail.com>
2016-11-12 16:09:08 -0800

    Add dummy topic for testing stream processor

--- script/create_topics.sh
+++ script/create_topics.sh
@@ -37,3 +37,13 @@ docker exec omniscience_kafka_1 /opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh \
   --config retention.ms=9223372036854775807 \
   --config retention.bytes=-1 \
   --config flush.messages=1
+
+docker exec omniscience_kafka_1 /opt/kafka_2.11-0.10.1.0/bin/kafka-topics.sh \
+  --zookeeper zookeeper:2181 \
+  --create \
+  --topic my-output-topic \
+  --partitions 1 \
+  --replication-factor 1 \
+  --config retention.ms=9223372036854775807 \
+  --config retention.bytes=-1 \
+  --config flush.messages=1
