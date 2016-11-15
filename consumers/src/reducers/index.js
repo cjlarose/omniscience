f46d326c464e89a19a58daa5b7648a0caf8e8798
Chris LaRose <cjlarose@gmail.com>
2016-11-14 22:17:50 -0800

    Delete old consumers

--- consumers/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-const { annotatePushEvent } = require('./push_event_annotation');
-const { handleOffsets } = require('./offsets');
-
-module.exports = {
-  pushEventAnnotation: annotatePushEvent,
-  offsets: handleOffsets,
-};
